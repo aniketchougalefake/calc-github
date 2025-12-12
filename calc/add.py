2a19515820964f2609d0535c220b4de373fb2ddf
dai18 <dai18@gmail.com>
2025-12-12 15:16:23 +0530

    ADD & SUB & MUL

--- calc/add.py
+++ calc/add.py
@@ -1,7 +1,4 @@
 a=int(input("Enter First No"))
 b=int(input("Enter Secound No"))
-<<<<<<< HEAD
 print(f"Addition {a+b},Substraction {a-b}"
-=======
 print(f"Addition {a+b} , Multiplication {a*b}"
->>>>>>> 4c43bfa0c03b3c0e2f68a88ef21fe486f358b9dc
